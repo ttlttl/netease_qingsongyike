fca24341dfabce7c00c12066967b8877c7a08c79
ttlttl <wangmingape@gmail.com>
2015-11-29 21:43:24 +0800

    Add some html pages

--- hello.py
+++ hello.py
@@ -8,14 +8,27 @@ app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/qsyk')
+def qsyk():
     session = DBSession()
     qingsongyike = session.query(Qingsongyike).first()
     session.close()
     title=qingsongyike.title
     body=qingsongyike.body
-    return render_template('index.html',title=title, body=body)
+    return render_template('qsyk.html',title=title, body=body)
+
 
 if __name__ == '__main__':
     manager.run()
