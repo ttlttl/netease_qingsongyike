3eebacf0833756227211b8fddc1eb1672f1637d7
ttlttl <wangmingape@gmail.com>
2015-11-29 21:16:43 +0800

    hello, bootstrap

--- hello.py
+++ hello.py
@@ -1,14 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 from downloader.db_function import DBSession
 from downloader.models import Qingsongyike
 
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
     session = DBSession()
-    body = session.query(Qingsongyike.body).first()
-    return body
+    qingsongyike = session.query(Qingsongyike).first()
+    session.close()
+    title=qingsongyike.title
+    body=qingsongyike.body
+    return render_template('index.html',title=title, body=body)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
