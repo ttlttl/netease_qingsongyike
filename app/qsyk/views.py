f353162119f3724b35b2d16430fcce59b6c16280
ttlttl <wangmingape@gmail.com>
2015-12-06 14:49:26 +0800

    Order by ptime desc

--- app/qsyk/views.py
+++ app/qsyk/views.py
@@ -1,11 +1,12 @@
 from flask import render_template, request
+from sqlalchemy import desc
 from ..models import Qingsongyike
 from . import qsyk
 
 @qsyk.route('/qingsong')
 def qingsong():
     page = request.args.get('page', 1, type=int)
-    pagination = Qingsongyike.query.order_by(Qingsongyike.id).paginate(
+    pagination = Qingsongyike.query.order_by(desc((Qingsongyike.ptime))).paginate(
         page, per_page=1, error_out=False
     )
     articles = pagination.items
