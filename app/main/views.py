aa138ae287979b671ac8c43241c3a13951f72d84
ttlttl <wangmingape@gmail.com>
2015-12-10 00:25:20 +0800

    Add weather forecast support

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from . import main
+from .weather import weather
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', weather=weather)
