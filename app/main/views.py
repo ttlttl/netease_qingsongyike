298bdf4115c0fdf6b2b8556747bf508c0219c94a
ttlttl <wangmingape@gmail.com>
2015-12-10 20:07:40 +0800

    Weather: fix bug

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template
 from . import main
-from .weather import weather
+from .weather import get_weather
 
 @main.route('/')
 def index():
+    weather = get_weather('shanghai')
     return render_template('index.html', weather=weather)
--- app/main/weather.py
+++ app/main/weather.py
@@ -1,11 +1,13 @@
 import json
 from urllib.request import Request, urlopen
 
-url = 'http://apis.baidu.com/heweather/weather/free?city=shanghai'
-req = Request(url)
-req.add_header("apikey", "ebfc3a7186254a37fc715d2f9d4c4049")
-resp = urlopen(req)
-content = resp.read()
-data = content.decode('utf-8')
-j = json.loads(data)
-weather = j["HeWeather data service 3.0"][0]
+def get_weather(city):
+    url = 'http://apis.baidu.com/heweather/weather/free?city=%s' % city
+    req = Request(url)
+    req.add_header("apikey", "ebfc3a7186254a37fc715d2f9d4c4049")
+    resp = urlopen(req)
+    content = resp.read()
+    data = content.decode('utf-8')
+    j = json.loads(data)
+    weather = j["HeWeather data service 3.0"][0]
+    return weather
